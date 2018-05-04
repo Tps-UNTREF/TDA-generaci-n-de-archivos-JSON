2a1ca672eca08f2576cfe64ec1e92b24c99ef85c
matt1bell <bellotti.matias.37996271@gmail.com>
2018-05-04 01:31:30 -0300

    Se creo firma de otro metedo mas.Se trato de hacer un metodo: tira warning al darle valores a las variables. El struct photo_info se rompe al compilar.

--- src/Json.c
+++ src/Json.c
@@ -1,4 +1,32 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "Json.h"
+
+void generarJson(Json* this){
+	this->size = "bytes";
+	this->hash= "37eb1ba1849d4b0fb0b28caf7ef3af52";
+	this->bytes = 056;
+	this->thumb_exists = "false";
+	this->rev = "714f029684fe";
+	this->modified = "Wed, 27 Apr 2011 22:18:51 +0000";
+	this->path = "/Photos";
+	this->is_dir = "true";
+	this->icon = "folder";
+	this->root = "dropbox";
+	this->photo_info->lat_long = 2453963;
+	this->contents->size = "2.3 MB";
+	this->contents->rev = "38af1b183490";
+	this->contents->client_mtime = "Thu, 29 Aug 2013 01:12:02 +0000";
+	this->contents->modified = "Mon, 07 Apr 2014 23:13:16 +0000";
+	this->contents->path = "/Photos/flower.jpg";
+
+}
 
 
+
+
+void generarJsonEnArchivo(Json* json, char destino){
+
+}
--- src/Json.h
+++ src/Json.h
@@ -2,26 +2,27 @@
 #ifndef JSON_H_
 #define JSON_H_
 
-struct _Contents{
-	char* size,rev,thumb_exists,bytes,modified,client_mtime,path,is_dir,icon,root,mime_type;
+typedef struct _Contents{
+	char size,rev,thumb_exists,bytes,modified,client_mtime,path,is_dir,icon,root,mime_type;
 	int revision;
-	struct _Photo_info photo_info;
-};
+}_C;
 
 struct _Photo_info{
-	double* lat_long[];
-	char* time_taken;
+	double lat_long[16];
+	char time_taken;
 };
 
 typedef struct _Json {
-	char* size,hash,thumb_exists,rev,modified, path,is_dir,icon,root;
+	char size,hash,thumb_exists,rev,modified, path,is_dir,icon,root;
 	int bytes,revision;
+	struct _Photo_info photo_info;
 	struct _Contents contents;
 
 }Json;
 
 
-void generarJson(Json* json);
+void generarJson(Json* this);
+void generarJsonEnArchivo(Json* this, char destino);
 
 
 #endif /* JSON_H_ */
